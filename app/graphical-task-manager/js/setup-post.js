f7c866d898e6cc3fc8d33c1002183d4940109afc
Davíð Helgason <david@reiknir.it>
2020-04-03 16:49:04 +0000

    Remove everything

--- app/graphical-task-manager/js/setup-post.js
+++
@@ -1,6 +0,0 @@
-    // Warning before leaving the page (back button, or outgoinglink)
-    window.onbeforeunload = function () {
-        return "Do you really want to leave?";
-        //if we return nothing here (just calling return;) then there will be no pop-up question at all
-        //return;
-      };
