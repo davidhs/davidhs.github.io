71549cd2d43a27c2fdb57e47bab047939fe00f9d
Davíð Helgason <david@eldeysoft.is>
2024-02-09 14:58:20 +0000

    .

--- programs.js
+++
@@ -1,9 +0,0 @@
-import "./js/3rd/solid/solid.js";
-import "./js/3rd/solid/html.js";
-import { render } from "./js/3rd/solid/web.js";
-
-import Page from "./src/pages/program.solid.js";
-
-window.addEventListener('load', (event) => {
-    render(Page, document.body);
-});
